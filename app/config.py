edf4ef6e1cea58c3ba2a1a43763f8a28ce1f0e6d
Andres Alonso <108694498+AXAStudio@users.noreply.github.com>
2025-08-13 16:26:26 -0400

    bug fix

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ API Config
 MAX_ARTICLE_COUNT = 10
 MODEL_VARIANTS = {"light", "medium", "ensemble"}
 # MODEL_FILE_FORMAT = "model_{}.keras" # windows
-MODEL_FILE_FORMAT_UNIX = "model_{}_utf8.keras" # unix
+MODEL_FILE_FORMAT = "model_{}_utf8.keras" # unix
